b212287b890a0c7ea95cfe8778eca0e964faa243
Sid Mitra <sidmitra.del@gmail.com>
2021-05-05 12:43:04 -0400

    wip

--- eventbusk/cli.py
+++ eventbusk/cli.py
@@ -1,8 +1,45 @@
 """
-CLI Interface
+Command Line Interface
 """
+from __future__ import annotations
 
 import click
+import importlib
+from types import ModuleType
+
+from eventbusk import EventBus
+
+
+
+def find_app(app: str, attr_name: str="app") -> EventBus:
+    """
+    Import an EventBus instance based on a path name.
+
+    Arguments
+    ----------
+    app: str
+        Path to a module containing an EventBus instance.
+
+    Keyword Arguments
+    ------------------
+    attr_name: str
+       Name of the EventBus instance inside the app. Defaults to 'app'.
+    """
+    if ':' in app:
+        module_name, attr_name = app.split(":")
+    else:
+        module_name = app
+
+    module = importlib.import_module(module_name)
+    if not isinstance(module, types.ModuleType):
+        raise AttributeError(f"Module f{module_name} not found or cannot be imported")
+
+    # Find bus instance within the module
+    found = getattr(module, attr_name)
+    if not isinstance(found, EventBus):
+        raise AttributeError(f"EventBus instance {attr_name} not found in {module_name}")
+
+    return found
 
 
 @click.group()
@@ -11,15 +48,15 @@ def cli():
 
 
 @cli.command()
-@click.option('--app', help="Path to EventBus instance. eg. 'myapp:bus'")
+@click.option('--app', help="Path to EventBus instance. eg. 'mymodule:app'")
 def worker(app):
     """
     Start consumer workers
     """
-    click.echo(app)
-    #
-    agents = get_agents()
+    bus = find_app(app)
+    click.echo(bus)
 
+    agents = bus.agents
     num_workers = len(agents)
     if len(agents) > 0:
         with concurrent.futures.ProcessPoolExecutor(
