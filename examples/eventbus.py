a95727fffc326dd920c6996746205da21c79d141
Sid Mitra <sidmitra.del@gmail.com>
2021-05-13 17:58:17 -0400

    WIP

--- examples/eventbus.py
+++ examples/eventbus.py
@@ -1,3 +1,14 @@
+"""
+Run workers for the receivers via
+
+$>eventbusk worker -A eventbus:bus
+
+To send an event on the bus you can open a separate python shell
+
+>>> from eventbus import bus, Foo
+>>> foo=Foo(foo=1)
+>>> bus.send(foo)
+"""
 import logging
 from dataclasses import dataclass
 
@@ -6,7 +17,6 @@ import daiquiri
 daiquiri.setup(level=logging.INFO)
 
 logger = daiquiri.getLogger(__name__)
-
 
 from eventbusk import Event, EventBus
 
@@ -25,7 +35,6 @@ class Bar(Event):
 
 bus.register_event("topic_foo", Foo)
 bus.register_event("topic_bar", Bar)
-
 
 # Consume an event
 @bus.agent(event_type=Foo)
